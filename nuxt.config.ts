b8da24a6c33cbd466b2bf2a0cf4997dd58c04eec
shellingfordly <shellingfordly@qq.com>
2024-02-05 11:29:11 +0800

    feat: add iconify icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        { src: 'https://code.iconify.design/3/3.1.1/iconify.min.js', defer: true },
+      ],
+    }
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", '@vueuse/nuxt', "shadcn-nuxt"],
   imports: {
@@ -8,4 +15,5 @@ export default defineNuxtConfig({
     componentDir: "./components/ui",
   },
   css: ["/styles/tailwind.css"],
+
 });
