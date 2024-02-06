11e75c3e2878e524ad7069261ea9bc5a095e2d56
shellingfordly <shellingfordly@qq.com>
2024-02-06 18:05:44 +0800

    chore: update deps

---
+++ layouts/default/menu-item.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const props = defineProps<{ menu: { name: string; path: string } }>();
+const route = useRoute();
+
+const className = computed(() =>
+  route.path === props.menu.path ? "bg-accent" : ""
+);
+</script>
+<template>
+  <div :class="[className, 'w-full p-3 rounded-md text-sm hover:bg-accent']">
+    <NuxtLink class="text-left w-full h-full" :to="menu.path">
+      {{ menu.name }}
+    </NuxtLink>
+  </div>
+</template>
+<style scoped></style>
